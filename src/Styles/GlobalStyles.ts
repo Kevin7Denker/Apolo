77c1c88e6bb8564e82bf52621865211ea2f5b661
Kevin Denker <oficialdenker@gmail.com>
2024-05-09 22:55:02 -0300

    fix

--- src/Styles/GlobalStyles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const ContainerExemplo = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 20px;
-`;

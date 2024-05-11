1c4c893c6740d6e2a105881a22e6f06a823c6d71
Kevin Denker <oficialdenker@gmail.com>
2024-05-11 17:32:28 -0300

    feat

--- src/Routes/RoutesApp.tsx
+++ src/Routes/RoutesApp.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../Views/Home/Home";
 import Error from "../Views/Error/Error";
 import SignIn from "../Views/SignIn/SignIn";
+import Soon from "../Views/Soon/Soon";
 
 export default function RoutesApp() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<Error />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Soon />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/signIn" element={<SignIn />} />
       </Routes>
     </BrowserRouter>

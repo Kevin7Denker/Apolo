957c04937af0cb931094cb9fa0d9a67cd7b5802a
Kevin Denker <oficialdenker@gmail.com>
2024-05-16 22:04:02 -0300

    feat

---
+++ src/Hooks/Contexts/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../Providers/AuthProvider";
+
+interface ProtectedRouteProps {
+  element: React.ReactElement;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
+  const { state } = useAuth();
+
+  console.log(state);
+
+  if (!state.isLoggedIn) {
+    return <Navigate to="/welcome" replace />;
+  }
+
+  return element;
+};
+
+export default ProtectedRoute;

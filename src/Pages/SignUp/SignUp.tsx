7fd49c8819de237a7ff8f8a4caa7bcee00b98491
Kevin Denker <oficialdenker@gmail.com>
2024-05-20 18:12:45 -0300

    feat

--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -1,4 +1,23 @@
+import { Link } from "react-router-dom";
+import BackgroundHome from "../../Components/backgroundHome";
 import UserProfile from "../../Utils/UserProfile";
+import {
+  Container,
+  Content,
+  IntroTexts,
+  IntroTitle,
+  IntroSubtitle,
+  Submit,
+  Form,
+  MainRegisterText,
+  SubRegisterText,
+  SubText,
+  RegisterContainer,
+  Input,
+  Label,
+  InputContent,
+} from "./Styles/Index";
+import { InputContainer } from "../Welcome/Styles/Index";
 
 const SignUp = () => {
   const profile = UserProfile();
@@ -6,7 +25,89 @@ const SignUp = () => {
   console.log(profile);
   return (
     <>
-      <h1>SignUp</h1>
+      <BackgroundHome />
+      <Content>
+        <Container>
+          <IntroTexts>
+            <IntroTitle>Sign Up</IntroTitle>
+            <IntroSubtitle>
+              Enter your details below to create your account and get started.
+            </IntroSubtitle>
+          </IntroTexts>
+          <Form>
+            <InputContent>
+              <InputContainer>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                  required
+                />
+                <Label>Name</Label>
+              </InputContainer>
+              <InputContainer>
+                <Input
+                  type="password"
+                  autoComplete="current-password"
+                  placeholder="Password"
+                  required
+                />
+                <Label>Surname</Label>
+              </InputContainer>
+            </InputContent>
+            <InputContent>
+              <InputContainer>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                  required
+                />
+                <Label>Phone</Label>
+              </InputContainer>
+              <InputContainer>
+                <Input
+                  type="password"
+                  autoComplete="current-password"
+                  placeholder="Password"
+                  required
+                />
+                <Label>Nationality</Label>
+              </InputContainer>
+            </InputContent>
+            <InputContent>
+              <InputContainer>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                  required
+                />
+                <Label>Password</Label>
+              </InputContainer>
+              <InputContainer>
+                <Input
+                  type="password"
+                  autoComplete="current-password"
+                  placeholder="Password"
+                  required
+                />
+                <Label>Confirm Password</Label>
+              </InputContainer>
+            </InputContent>
+            <Submit>Confirm</Submit>
+          </Form>
+
+          <RegisterContainer>
+            <MainRegisterText>Already have an account?</MainRegisterText>
+            <SubRegisterText>
+              <Link to="/welcome">
+                <SubText>Sign In!</SubText>
+              </Link>
+            </SubRegisterText>
+          </RegisterContainer>
+        </Container>
+      </Content>
     </>
   );
 };

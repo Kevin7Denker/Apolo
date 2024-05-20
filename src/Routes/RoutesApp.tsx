7eec148418e746c8853c97240f0fb40f5044ef90
Kevin Denker <oficialdenker@gmail.com>
2024-05-20 18:12:56 -0300

    feat

--- src/Routes/RoutesApp.tsx
+++ src/Routes/RoutesApp.tsx
@@ -3,19 +3,18 @@ import Welcome from "../Pages/Welcome/Welcome";
 import Error from "../Pages/Error/Error";
 import Soon from "../Pages/Soon/Soon";
 import SignUp from "../Pages/SignUp/SignUp";
-import ProtectedRoute from "../Hooks/Contexts/ProtectedRoute";
+//import ProtectedRoute from "../Hooks/Contexts/ProtectedRoute";
 
 export default function RoutesApp() {
+  //<Route path="/dashboard" element={<ProtectedRoute element={} />} />
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<Error />} />
         <Route path="/" element={<Soon />} />
         <Route path="/welcome" element={<Welcome />} />
-        <Route
-          path="/signup"
-          element={<ProtectedRoute element={<SignUp />} />}
-        />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );

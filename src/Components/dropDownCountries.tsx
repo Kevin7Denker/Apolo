6c6b80ca38e49a582c950a04498abec9f32f05dc
Kevin Denker <oficialdenker@gmail.com>
2024-05-21 16:00:34 -0300

    fix

--- src/Components/dropDownCountries.tsx
+++ src/Components/dropDownCountries.tsx
@@ -5,8 +5,6 @@ import { countries } from "../Utils/Data/Countries";
 const DropdownWrapper = styled.div`
   position: relative;
   width: 100%;
-  border: none;
-  outline: none;
 `;
 
 const DropdownLabel = styled.label`
@@ -14,24 +12,23 @@ const DropdownLabel = styled.label`
   margin-bottom: 0.5rem;
   border: none;
   outline: none;
+  width: 100%;
   background-color: black;
 `;
 
 const DropdownSelect = styled.select`
-  width: 100%;
-  padding: 1rem;
-  background-color: black;
-  font-size: 0.8rem;
-  border: 1px solid white;
-  outline: none;
-  border-radius: 5px;
   appearance: none;
   -webkit-appearance: none;
   -moz-appearance: none;
-
-  background: url("/path/to/arrow-down.png") no-repeat right 10px center;
-  background-size: 16px; /* Ajuste conforme necessário */
-  padding-right: 30px; /* Espaço para a seta */
+  width: 100%;
+  padding: 10px;
+  border: 1px solid white;
+  background-color: black;
+  background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMCIgaGVpZ2h0PSI2IiB2aWV3Qm94PSIwIDAgMTAgNiI+PHBvbHlnb24gcG9pbnRzPSIwLDAgNSw2IDEwLDAiIHN0eWxlPSJmaWxsOiM2NjY7IiAvPjwvc3ZnPg==");
+  background-repeat: no-repeat;
+  background-position: right 10px center;
+  background-size: 10px;
+  border-radius: 4px;
 `;
 
 const CountryDropdown = ({
@@ -47,7 +44,9 @@ const CountryDropdown = ({
     <DropdownWrapper>
       <DropdownLabel htmlFor={name}>{label}</DropdownLabel>
       <DropdownSelect id={name} name={name} onChange={onChange}>
-        <option value="">{label}</option>
+        <option className="option" value="">
+          {label}
+        </option>
         {countries.map((country) => (
           <option key={country.code} value={country.code}>
             {country.name}

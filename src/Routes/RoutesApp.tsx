cbfe4b5399e4a667e0675a0047351f341f84a14e
Kevin Denker <oficialdenker@gmail.com>
2024-07-10 20:30:35 -0300

    Update RoutesApp.tsx

--- src/Routes/RoutesApp.tsx
+++ src/Routes/RoutesApp.tsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Welcome from "../Pages/Welcome/Welcome";
+import SignIn from "../Pages/SignIn/SignIn";
 import Error from "../Pages/Error/Error";
 import SignUp from "../Pages/SignUp/SignUp";
 import ProtectedRoute from "../Hooks/Contexts/ProtectedRoute";
+import Home from "../Pages/Home/Home";
+import Welcome from "../Pages/Welcome/Welcome";
 
 export default function RoutesApp() {
-  //<Route path="/dashboard" element={<ProtectedRoute element={} />} />
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<Error />} />
-        <Route path="/" element={<ProtectedRoute element={<Welcome />} />} />
+        <Route path="/" element={<ProtectedRoute element={<SignIn />} />} />
         <Route
           path="/signup"
           element={<ProtectedRoute element={<SignUp />} />}
         />
+        <Route
+          path="/welcome"
+          element={<ProtectedRoute element={<Welcome />} />}
+        />
+        <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
       </Routes>
     </BrowserRouter>
   );

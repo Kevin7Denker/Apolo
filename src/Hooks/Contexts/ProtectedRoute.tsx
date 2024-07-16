c8922baf23ca58d9944684b8e5b032af6bfd6922
Kevin Denker <oficialdenker@gmail.com>
2024-07-16 11:41:33 -0300

    Update ProtectedRoute.tsx

--- src/Hooks/Contexts/ProtectedRoute.tsx
+++ src/Hooks/Contexts/ProtectedRoute.tsx
@@ -1,22 +1,35 @@
+import React, { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../Providers/AuthProvider";
+import { useAuth } from "../useAuth";
 
 interface ProtectedRouteProps {
   element: React.ReactElement;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
-  const { state } = useAuth();
+  const state = useAuth();
   const location = useLocation();
-  const { isLoggedIn } = state;
+  const [shouldRedirect, setShouldRedirect] = useState<boolean>(false); // Inicializado como false
 
-  const allowedRoutes = ["/", "/signup"];
+  useEffect(() => {
+    const isLoggedIn = state;
 
-  if (!isLoggedIn && !allowedRoutes.includes(location.pathname)) {
-    return <Navigate to="/" replace />;
+    if (isLoggedIn && location.pathname === "/") {
+      setShouldRedirect(true);
+    } else if (
+      !isLoggedIn &&
+      location.pathname !== "/" &&
+      location.pathname !== "/signup"
+    ) {
+      setShouldRedirect(false);
+    }
+  }, [state, location]);
+
+  if (shouldRedirect) {
+    return <Navigate to="/home" replace />;
+  } else {
+    return element;
   }
-
-  return element;
 };
 
 export default ProtectedRoute;

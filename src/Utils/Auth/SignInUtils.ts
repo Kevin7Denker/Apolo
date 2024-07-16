85447cdd9e7d63a9ff5880a55ea9b0173c9b2288
Kevin Denker <oficialdenker@gmail.com>
2024-07-16 11:42:00 -0300

    Update SignInUtils.ts

--- src/Utils/Auth/SignInUtils.ts
+++ src/Utils/Auth/SignInUtils.ts
@@ -21,14 +21,14 @@ export const handleResponse = (
 ): void => {
   const user = res?.data.items[0].user;
   if (user) {
-    Cookies.set("user", JSON.stringify(user), { expires: 7 });
-    Cookies.set("isLoggedIn", "true", { expires: 7 });
-
-    dispatch({ type: "LOGIN", payload: user });
-
     if (user.profile.dateCreation == null) {
       navigate("/welcome");
     } else {
+      Cookies.set("user", JSON.stringify(user), { expires: 7 });
+      Cookies.set("isLoggedIn", "true", { expires: 7 });
+
+      dispatch({ type: "LOGIN", payload: user });
+
       navigate("/dashboard");
     }
   }

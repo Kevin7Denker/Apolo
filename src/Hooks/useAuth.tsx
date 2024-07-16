a2a83b5bf76dcedb78f842ffe1986740e1fb60df
Kevin Denker <oficialdenker@gmail.com>
2024-07-16 12:06:34 -0300

    Update useAuth.tsx

--- src/Hooks/useAuth.tsx
+++ src/Hooks/useAuth.tsx
@@ -3,6 +3,7 @@ import { AuthContext } from "./Providers/AuthProvider";
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
+  console.log(context);
   if (context === undefined) {
     throw new Error("useAuth must be used within an AuthProvider");
   }

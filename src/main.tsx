ce402cbbe458f230127b36c34d1c1085f4f394eb
Kevin Denker <oficialdenker@gmail.com>
2024-07-16 11:41:43 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./Styles/GlobalStyles.scss";
-import { AuthProvider } from "./Hooks/Providers/AuthProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);

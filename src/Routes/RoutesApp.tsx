53c364de96f0812cde85ceaadf77501f85c09c5f
Kevin Denker <oficialdenker@gmail.com>
2024-07-16 12:06:40 -0300

    Update RoutesApp.tsx

--- src/Routes/RoutesApp.tsx
+++ src/Routes/RoutesApp.tsx
@@ -5,26 +5,20 @@ import SignUp from "../Pages/SignUp/SignUp";
 import ProtectedRoute from "../Hooks/Contexts/ProtectedRoute";
 import Home from "../Pages/Home/Home";
 import Welcome from "../Pages/Welcome/Welcome";
-import { AuthProvider } from "../Hooks/Providers/AuthProvider";
 
 export default function RoutesApp() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<ProtectedRoute element={<Error />} />} />
-          <Route path="/" element={<ProtectedRoute element={<SignIn />} />} />
-          <Route
-            path="/signup"
-            element={<ProtectedRoute element={<SignUp />} />}
-          />
-          <Route
-            path="/welcome"
-            element={<ProtectedRoute element={<Welcome />} />}
-          />
-          <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/welcome"
+          element={<ProtectedRoute element={<Welcome />} />}
+        />
+        <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }

87266b5787c5175f8eab48a8273ad12a1adaa73c
Kevin Denker <oficialdenker@gmail.com>
2024-07-16 12:06:37 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./Styles/GlobalStyles.scss";
+import { AuthProvider } from "./Hooks/Providers/AuthProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);

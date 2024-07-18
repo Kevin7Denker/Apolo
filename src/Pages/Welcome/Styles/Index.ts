237a6ded03b3a173f1184beb61ab617d2eb7f47c
Kevin Denker <oficialdenker@gmail.com>
2024-07-18 00:12:34 -0300

    Update Index.ts

--- src/Pages/Welcome/Styles/Index.ts
+++ src/Pages/Welcome/Styles/Index.ts
@@ -9,13 +9,74 @@ export const Modal = styled.div`
   color: white;
   padding: 20px;
   border-radius: 10px;
+  background-color: rgb(5, 5, 5);
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  max-width: 30svw;
+
+  @media (width <= 650px) {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    max-width: 80svw;
+  }
+
+  @media (width > 650px) and (width <= 900px) {
+    max-width: 50svw;
+  }
+`;
+
+export const Title = styled.h1``;
+
+export const Subtitle = styled.h2`
+  font-weight: 600;
+`;
+
+export const Text = styled.p`
+  @media (width <= 650px) {
+    text-align: justify;
+  }
 `;
 
 export const ListStyle = styled.div`
   max-height: 200px;
   overflow-y: scroll;
-  border: 1px solid #ccc;
-  padding: 10px;
+
+  ul {
+    cursor: pointer;
+
+    :hover {
+      background-color: rgb(25, 25, 25);
+    }
+  }
+
+  li {
+    border-radius: 5px;
+    cursor: pointer;
+    padding: 5px;
+
+    &.selected {
+      background-color: rgb(35, 35, 35);
+    }
+
+    :focus {
+      background-color: rgb(25, 25, 25);
+    }
+  }
+
+  @media (width <= 650px) {
+    li {
+      padding: 5px;
+    }
+  }
+
+  @media (width > 650px) and (width <= 900px) {
+    max-width: 50svw;
+  }
 `;
 
 export const Content = styled.div`
@@ -27,18 +88,20 @@ export const Content = styled.div`
 `;
 
 export const Submit = styled.button`
-  width: 30%;
+  background-color: ${(props) =>
+    props.disabled ? "rgb(45,45,45)" : "var(--primary-color)"};
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  max-width: 30%;
   padding: 0.8rem;
   margin-top: 2rem;
   border-radius: 5px;
-  background-color: var(--primary-color);
   border: none;
   font-size: 1rem;
-  cursor: pointer;
   transition: 0.5s;
 
   &:hover {
-    background-color: var(--primary-color-dark);
+    background-color: ${(props) =>
+      props.disabled ? "rgb(65,65,65)" : "var(--primary-color-dark)"};
     transition: 0.5s;
   }
 `;

12ffe5faf0273ea0668fd1289d6f1735a756e979
Kevin Denker <oficialdenker@gmail.com>
2024-07-31 22:17:13 -0300

    Update userRepository.ts

--- src/Repository/userRepository.ts
+++ src/Repository/userRepository.ts
@@ -1,5 +1,27 @@
+import axios from "axios";
+import UserValidation from "../services/validations/userValidations";
+
 class UserRepository {
-  // Adicinar um websocket para verificar se o identity é válido
+  private urlIdentity;
+
+  private userValidation = new UserValidation();
+
+  constructor() {
+    this.urlIdentity = `https://apolo-api.onrender.com/user/find-identity/`;
+  }
+
+  async findIdentity(id: string) {
+    try {
+      const response = await axios.post(`${this.urlIdentity}` + `${id}`);
+
+      return response;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error:", error);
+        throw error;
+      }
+    }
+  }
 }
 
 export default UserRepository;

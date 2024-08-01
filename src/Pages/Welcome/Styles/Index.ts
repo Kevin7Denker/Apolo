5140f2de60cd1ebba2e181845819a27215fe92f5
Kevin Denker <oficialdenker@gmail.com>
2024-07-31 22:36:58 -0300

    Update Index.ts

--- src/Pages/Welcome/Styles/Index.ts
+++ src/Pages/Welcome/Styles/Index.ts
@@ -13,6 +13,31 @@ export const Modal = styled.div`
   background-color: rgb(5, 5, 5);
 `;
 
+export const ErrorMessage = styled.div`
+  position: absolute;
+  bottom: 0;
+  background-color: red;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  transform: translateY(-100px);
+  z-index: 1000;
+  transition: opacity 0.5s ease-in-out;
+  opacity: 1;
+  animation: fadeOut 1s;
+
+  @keyframes fadeOut {
+    0% {
+      opacity: 0;
+      transform: translateY(0px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(-100px);
+    }
+  }
+`;
+
 export const ModalContent = styled.div`
   display: flex;
   flex-direction: column;
@@ -31,9 +56,11 @@ export const ModalContent = styled.div`
 `;
 
 export const Content = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
 `;
